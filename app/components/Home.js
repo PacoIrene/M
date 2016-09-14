182dccd5a930fde7d650ebe470f6b038d0358a3e
PacoIrene <zhenyangchu@gmail.com>
2016-09-14 17:37:19 +0800

    drop rnd

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router';
 import classNames from 'classNames';
 import Tree from './Tree';
 import EditorAndPreview from './EditorAndPreview';
-import Rnd from 'react-rnd';
 import './Home.scss';
 
 
@@ -26,27 +25,6 @@ export default class Home extends Component {
           currentFilePath={this.props.currentFilePath}
           toggleExpandState={this.props.toggleExpandState}
           openFile={this.props.openFile} />
-        <Rnd
-          initial={{
-            x: 200,
-            y: 0,
-            width: 1,
-            height: window.innerHeight,
-          }}
-          bounds={'parent'}
-          className='draggable-bound'
-          isResizable={{
-            top:false,
-            right:false,
-            bottom:false,
-            left:false,
-            topRight:false,
-            bottomRight:false,
-            bottomLeft:false,
-            topLeft:false
-          }}
-          moveAxis='x'
-          onDrag={this.resizeBound.bind(this)}></Rnd>
         <EditorAndPreview
           style={{
             marginLeft: treeWidth
@@ -57,12 +35,5 @@ export default class Home extends Component {
           syncContent={this.props.syncContent}/>
       </div>
     );
-  }
-
-  resizeBound(event, ui) {
-    const {left} = ui.position;
-    this.setState({
-      treeWidth: left
-    });
   }
 }
